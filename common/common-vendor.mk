751e025f3fa175b1e6d67e0c9850297329c6bf13
razorloves <razorloves@gmail.com>
2019-01-20 23:24:40 -0600

    Add google-hiddenapi-package-whitelist.xml
    Pulled from marlin pie factory image PQ1A.190105.004
    
    Change-Id: I16fdfb26ac845a42e445b0535a16ecb39088e617

--- common/common-vendor.mk
+++ common/common-vendor.mk
@@ -19,6 +19,7 @@ PRODUCT_COPY_FILES += \
     vendor/gapps/common/proprietary/etc/permissions/com.google.android.dialer.support.xml:system/etc/permissions/com.google.android.dialer.support.xml \
     vendor/gapps/common/proprietary/etc/permissions/com.google.android.maps.xml:system/etc/permissions/com.google.android.maps.xml \
     vendor/gapps/common/proprietary/etc/permissions/privapp-permissions-google.xml:system/etc/permissions/privapp-permissions-google.xml \
+    vendor/gapps/common/proprietary/etc/sysconfig/google-hiddenapi-package-whitelist.xml:system/etc/sysconfig/google-hiddenapi-package-whitelist.xml \
     vendor/gapps/common/proprietary/etc/sysconfig/google.xml:system/etc/sysconfig/google.xml \
     vendor/gapps/common/proprietary/etc/sysconfig/google_build.xml:system/etc/sysconfig/google_build.xml
 
