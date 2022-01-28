3adfb82419f75e8399c3b699ba66b5c71e5620b1
Alessandro Astone <ales.astone@gmail.com>
2022-01-28 13:44:28 +0100

    rro: build: Make x86 targets build

--- build/target/product/lineage_gapps_x86.mk
+++ build/target/product/lineage_gapps_x86.mk
@@ -2,4 +2,8 @@ include vendor/lineage/build/target/product/lineage_x86.mk
 
 PRODUCT_NAME := lineage_gapps_x86
 
+PRODUCT_SDK_ADDON_NAME :=
+PRODUCT_SDK_ADDON_SYS_IMG_SOURCE_PROP :=
+TARGET_NO_KERNEL_OVERRIDE := true
+
 PRODUCT_SOONG_NAMESPACES += vendor/gapps/overlay
