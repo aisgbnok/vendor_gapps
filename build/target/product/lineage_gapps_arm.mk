829c575db18b777899aa75f0edf5536b751bd9f5
Alessandro Astone <ales.astone@gmail.com>
2022-01-28 10:26:36 +0100

    build: rro: Don't use inherit-product
    It is broken in weird ways

--- build/target/product/lineage_gapps_arm.mk
+++ build/target/product/lineage_gapps_arm.mk
@@ -1,6 +1,5 @@
-$(call inherit-product, vendor/lineage/build/target/product/lineage_arm.mk)
+include vendor/lineage/build/target/product/lineage_arm.mk
 
 PRODUCT_NAME := lineage_gapps_arm
 
-PRODUCT_USE_DYNAMIC_PARTITIONS := false
 PRODUCT_SOONG_NAMESPACES += vendor/gapps/overlay
