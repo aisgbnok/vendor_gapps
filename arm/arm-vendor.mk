83a87f751d0be31c5f111c88a68c43b1d92a7790
Luca Stefani <luca.stefani.ge1@gmail.com>
2023-03-18 10:06:30 +0100

    Remove arm MarkupGoogle
    There's no fixed arm variant as of now.

--- arm/arm-vendor.mk
+++ arm/arm-vendor.mk
@@ -13,11 +13,7 @@ PRODUCT_PACKAGES += \
     Phonesky
 
 ifeq ($(TARGET_IS_GROUPER),)
-PRODUCT_COPY_FILES += \
-    vendor/gapps/arm/proprietary/product/app/MarkupGoogle/lib/arm/libsketchology_native.so:$(TARGET_COPY_OUT_PRODUCT)/app/MarkupGoogle/lib/arm/libsketchology_native.so
-
 PRODUCT_PACKAGES += \
-    MarkupGoogle \
     SpeechServicesByGoogle \
     talkback \
     Velvet \
